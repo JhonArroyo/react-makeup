0efbd9f316db6934afb84d002ca5a1b45d114a30
WhiteLotus <jhonsnipertf18@gmail.com>
2022-06-15 21:03:20 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import CrudForm from "./components/CrudForm";
+
+function App() {
+  return (
+    <div className="app">
+      <CrudForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CrudForm/CrudForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Products from './Products.json';
+
+const CrudForm = () => {
+    const [getProducts] = useState(Products);
+    return (
+        <div>
+            <h1>CRUD Form</h1>
+            <form>
+                <label>Product name:</label><br />
+                <input type="text" /><br />
+                <label>Quantity:</label><br />
+                <input type="text" /><br />
+                <label>Price:</label><br />
+                <input type="text" /><br />
+            </form>
+            <ul>
+                {getProducts.map(element => (
+                    <li key={element.id}>
+                        Product: {element.product}
+                        <br />
+                        Quantity: {element.quantity}
+                        <br />
+                        Price: {element.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default CrudForm;
